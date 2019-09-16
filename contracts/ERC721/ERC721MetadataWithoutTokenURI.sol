ff47848aec5d357452246b9979ca47ef9ec184b0
vince0656 <vince0656@googlemail.com>
2019-09-16 17:27:36 +0100

    Fixed some code coverage issues. Now up to 100%

--- contracts/ERC721/ERC721MetadataWithoutTokenURI.sol
+++ contracts/ERC721/ERC721MetadataWithoutTokenURI.sol
@@ -46,15 +46,4 @@ contract ERC721MetadataWithoutTokenURI is ERC165, ERC721, IERC721Metadata {
     function symbol() external view returns (string memory) {
         return _symbol;
     }
-
-    /**
-     * @dev Internal function to burn a specific token.
-     * Reverts if the token does not exist.
-     * Deprecated, use _burn(uint256) instead.
-     * @param owner owner of the token to burn
-     * @param tokenId uint256 ID of the token being burned by the msg.sender
-     */
-    function _burn(address owner, uint256 tokenId) internal {
-        super._burn(owner, tokenId);
-    }
 }
