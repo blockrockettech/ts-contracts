fa4cc7a05b62f051816a1174a99e37a9d0022b61
vince0656 <vince0656@googlemail.com>
2019-09-17 13:34:52 +0100

    Implemented ITwistedArtistCommissionRegistry

--- contracts/TwistedArtistCommissionRegistry.sol
+++ contracts/TwistedArtistCommissionRegistry.sol
@@ -3,10 +3,36 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./interfaces/ITwistedArtistCommissionRegistry.sol";
+import "./interfaces/ITwistedAccessControls.sol";
 
-contract TwistedArtistCommissionRegistry{
-    struct CommissionSplit {
-        uint256 percentage;
-        address payable recipient;
+contract TwistedArtistCommissionRegistry is ITwistedArtistCommissionRegistry {
+    using SafeMath for uint256;
+
+    ITwistedAccessControls accessControls;
+
+    address payable[] public artists;
+
+    // Artist address <> commission percentage
+    mapping(address => uint256) public artistCommissionSplit;
+
+    modifier isWhitelisted() {
+        require(accessControls.isWhitelisted(msg.sender), "Caller not whitelisted");
+        _;
+    }
+
+    constructor(ITwistedAccessControls _accessControls) public {
+        accessControls = _accessControls;
+    }
+
+    function getCommissionSplits() public view returns (uint256[] memory _percentages, address payable[] memory _recipients) {
+        require(artists.length > 0, "No artists have been registered");
+        _percentages = new uint256[](artists.length);
+        _recipients = new address payable[](artists.length);
+
+        for(uint256 i = 0; i < artists.length; i++) {
+            address payable artist = artists[i];
+            _percentages[i] = artistCommissionSplit[artist];
+            _recipients[i] = artist;
+        }
     }
 }
