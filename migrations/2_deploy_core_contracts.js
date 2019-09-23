2a8d6e205f0f83c84544b4b3f3c342c2e3aea849
vince0656 <vince0656@googlemail.com>
2019-09-23 16:16:31 +0100

    Added core contracts migration

---
+++ migrations/2_deploy_core_contracts.js
@@ -0,0 +1,50 @@
+const TwistedAccessControls = artifacts.require('TwistedAccessControls');
+const TwistedSisterToken = artifacts.require('TwistedSisterToken');
+const TwistedArtistCommissionRegistry = artifacts.require('TwistedArtistCommissionRegistry');
+const TwistedAuctionFundSplitter = artifacts.require('TwistedAuctionFundSplitter');
+const TwistedAuctionMock = artifacts.require('TwistedAuctionMock');
+const TwistedAuction = artifacts.require('TwistedAuction');
+
+function now(){ return Math.floor( Date.now() / 1000 ) }
+module.exports = async function (deployer, network, accounts) {
+    console.log("Deploying core contracts to network: " + network);
+
+    const creator = accounts[0];
+    const printingFund = accounts[1];
+    const baseIPFSURI = 'ipfs.io/ipns/';
+
+    await deployer.deploy(TwistedAccessControls, { from: creator });
+    const controls = await TwistedAccessControls.deployed();
+    console.log('controls.address:', controls.address);
+
+    await deployer.deploy(TwistedSisterToken, baseIPFSURI, controls.address, { from: creator });
+    const token = await TwistedSisterToken.deployed();
+    console.log('token.address:', token.address);
+
+    await deployer.deploy(TwistedArtistCommissionRegistry, controls.address, { from: creator });
+    const registry = await TwistedArtistCommissionRegistry.deployed();
+    console.log('registry.address:', registry.address);
+
+    await deployer.deploy(TwistedAuctionFundSplitter, registry.address, { from: creator });
+    const fundSplitter = await TwistedAuctionFundSplitter.deployed();
+    console.log('fundSplitter.address', fundSplitter.address);
+
+    if(network.toString() === '1') {
+        await deployer.deploy(TwistedAuction,
+            controls.address, token.address, fundSplitter.address, printingFund, now() + 5,
+            {
+                from: creator
+            });
+        const auction = await TwistedAuction.deployed();
+        console.log('auction.address:', auction.address);
+    } else {
+        // Deploy mock contract to test net
+        await deployer.deploy(TwistedAuctionMock,
+            controls.address, token.address, fundSplitter.address, printingFund, now() + 5,
+            {
+                from: creator
+            });
+        const auction = await TwistedAuctionMock.deployed();
+        console.log('auction.address:', auction.address);
+    }
+};
